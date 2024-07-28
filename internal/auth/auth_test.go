5ce790bc0d5a3db8b59616346318802e87a17771
Mjishu <128941980+Mjishu@users.noreply.github.com.>
2024-07-28 14:23:50 -0400

    added formating rules

--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -1,21 +1,21 @@
 package auth
 
 import (
-    "testing"
-    "net/http"
+	"net/http"
+	"testing"
 )
 
-func TestGetAPIKey(t *testing.T){
-    apiKey := "8213adsa21a213ax"
-    headers := http.Header{
-        "Authorization": []string{"ApiKey " + apiKey},
-    }
-    got,err := GetAPIKey(headers)
-    if err !=nil{
-        t.Fatalf("Unexpected error calling GetAPIKey, %v",err)
-    }
-    want := apiKey
-    if want != got {
-        t.Fatalf("expected: %v, got %v",want,got)
-    }
+func TestGetAPIKey(t *testing.T) {
+	apiKey := "8213adsa21a213ax"
+	headers := http.Header{
+		"Authorization": []string{"ApiKey " + apiKey},
+	}
+	got, err := GetAPIKey(headers)
+	if err != nil {
+		t.Fatalf("Unexpected error calling GetAPIKey, %v", err)
+	}
+	want := apiKey
+	if want != got {
+		t.Fatalf("expected: %v, got %v", want, got)
+	}
 }
